7b04c759468fb64d20a82de7bb35902d2035953e
SiddhantDeshmukh <sd486@exeter.ac.uk>
2020-11-20 17:55:48 +0100

    baseline metrics

--- scripts/baseline_metrics.py
+++ scripts/baseline_metrics.py
@@ -3,10 +3,24 @@ from .loss_functions import *
 import tensorflow.keras as keras
 
 
-def naive_forecast(data: np.array) -> float:
+def naive_forecast_start(data: np.array) -> float:
   # Choose the last value and use that as the forecast
   # Assumes 'data' is a 2D array (as input to TensorFlow)
   return data[:, -1]
+
+
+def naive_forecast_start(data: np.array) -> float:
+  # Choose the first value and use that as the forecast
+  # Assumes 'data' is a 2D array (as input to TensorFlow)
+  return data[:, 0]
+
+
+def mean_forecast(data: np.array) -> float:
+  # Choose the first value and use that as the forecast
+  # Assumes 'data' is a 2D array (as input to TensorFlow)
+  mean = np.mean(data, axis=1)
+
+  return mean
 
 
 def fully_connected_nn():
