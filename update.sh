c63f91ce96f1692a1971ba0e614a49acad672cff
SiddhantDeshmukh <sdeshmukh@lsw.uni-heidelberg.de>
2021-01-18 12:24:43 +0100

    new update script

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
-# Commit to GitHub
-echo "Pushing to GitHub..."
-git add .
-git commit -m "$1"
-git push
+# Add, commit and push to GitHub with supplied message
+
+read -p "Confirm: Add everything and push to GitHub? (y/n)" yn
+case $yn in
+  [Yy]* )
+    # Check for commit message
+    if [[ "$1" != "" ]]; then
+      MESSAGE="$1"
+    else
+      MESSAGE=$(date +"%Y-%m-%d %T")
+      echo "No commit message supplied, using date time $MESSAGE"
+    fi
+    git add .
+    git commit -m "$MESSAGE"
+    git push
+    ;;
+  [Nn]* ) exit;;
+  * ) echo "Please enter (y/n)"
+esac
